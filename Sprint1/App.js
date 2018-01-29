68787fbff24fc2ee38026258b24b32e68dce4a8d
Alex Garrett <31253740+Aydris@users.noreply.github.com>
2018-01-29 13:29:09 -0700

    Update App.js

--- Sprint1/App.js
+++ Sprint1/App.js
@@ -23,11 +23,11 @@ class Calculator extends React.Component {
     this.state = { 
       output: "",
       source: "",
-      sourceLa: "",
-      sourceLo: "",
+      sourceLa: 0,
+      sourceLo: 0,
       destination: "",
-      destinationLa: "",
-      destinationLo: "",
+      destinationLa: 0,
+      destinationLo: 0,
       unit: "miles"
     };
     this.calc = this.calc.bind(this);
@@ -38,50 +38,47 @@ class Calculator extends React.Component {
 
   updateSource(event) { /* updates the value of source */
     this.setState({source : event.target.value});
-    var inp = this.state.source;
-    if(inp.includes("N")){
-      inp = inp.split("N");
-      inp[0] += "N";
-    } else if(inp.includes("S")){
-      inp = inp.split("S");
-      inp[0] += "S";
+    var split = ""
+    if(this.state.source.includes("N")){
+      split = this.state.source.split("N");
+      split[0] += "N";
+      
+    }else if(this.state.source.includes("S")){
+      split = this.state.source.split("S");
+      split[0] += "S"
+    } else{
+      split = this.state.source.split(" ");
+    }
+    this.setState({sourceLa: this.degreesToDecimal(split[0])});
+    //The first character of split[1] is a space. Must remove it before degreesToDecimal will work.
+    this.setState({sourceLo: this.degreesToDecimal(split[1].substring(1, split[1].length))});
     
-    this.setState({sourceLa : this.degreesToDecimal(inp[0])});
-    this.setState({sourceLo : this.degreesToDecimal(inp[1])});
-    this.setState({output : this.state.sourceLa + this.state.sourceLo });
+    //Call the GCD method from here when ready.
+    this.setState({output: "TODO"});
   }
 
   updateDestination(event) { /* updates the value of destination */
-      this.setState({destination : event.target.value});
-    var inp = this.state.destination;
-    if(inp.includes("W")){
-      inp = inp.split("W");
-      inp[1] += "W";
-    } else if(inp.includes("E")){
-      inp = inp.split("E");
-      inp[1] += "E";
-    }
-    this.setState({destinationLa : this.degreesToDecimal(inp[0])});
-    this.setState({destinationLo : this.degreesToDecimal(inp[1])});
-    this.setState({output : this.state.sourceLa + this.state.sourceLo });
-  }
-  //Return decimal value of valid input.
-  degreesToDecimal(input){
-    var val = input.split(" ");
-    var ret = 0;
-    if(input.includes("°")){
-      ret += val[0].slice(0, -1);
+    this.setState({destination : event.target.value});
+    var split = ""
+    if(this.state.destination.includes("N")){
+      split = this.state.destination.split("N");
+      split[0] += "N";
       
-    } if(input.includes("\'")){
-      ret += val[1].slice(0, -1);
-    } if(input.includes("\"")){
-      ret += val[2].slice(0, -1);
+    }else if(this.state.destination.includes("S")){
+      split = this.state.destination.split("S");
+      split[0] += "S"
+    } else{
+      split = this.state.destination.split(" ");
     }
-    if(input.includes("S") || input.includes("W")){
-      ret *= -1;
-    }
-    return ret;
+    this.setState({destinationLa: this.degreesToDecimal(split[0])});
+    //The first character of split[1] is a space. Must remove it before degreesToDecimal will work.
+    this.setState({destinationLo: this.degreesToDecimal(split[1].substring(1, split[1].length))});
+    
+    //Call the GCD method from here when ready.
+    this.setState({output: "TODO"});
+    //this.setState({output : Number(event.target.value) + Number(this.state.source) });
   }
+
   unitConvert(event) { /* gets called when the select box's value is changed */
     /* currUnit = value of select box located ~line 86 */
     var currUnit = document.getElementById("unitSelect").value; 
@@ -97,6 +94,66 @@ class Calculator extends React.Component {
     }
   }
   
+  //Convert degrees to decimals
+  degreesToDecimal(degrees){
+    var inp = degrees.split(" ");
+    var ret = 0;
+    
+    if(degrees.includes("°")){
+      ret += Number(inp[0].slice(0, -1));
+    } else{
+      return Number(degrees);
+    }   
+    //minutes divided by 60
+    if(degrees.includes("\'")){
+      ret += (Number(inp[1].slice(0, -1)) / 60);
+    }
+    //seconds divided by 3600
+    if(degrees.includes("\"")){
+      ret += (Number(inp[2].slice(0,-1)) / 3600);
+    }
+    //If negative
+    if(degrees.includes("S") || degrees.includes("W")){
+      ret *= -1;
+    }
+    return ret;
+    
+  }
+    //Great Circle Distance
+  //Takes long1, lat1 (source) and long2, lat2 (destination) as floating point
+  GCD(long1, lat1, long2, lat2){
+    //0. Convert to radians
+    a1 = long1.toRadians;
+    b1 = lat1.toRadians;
+    a2 = long2.toRadians;
+    b2 = lat2.toRadians;
+    
+    //1. Compute X, Y, Z
+    x = Math.cos(b2)*Math.cos(a2) - Math.cos(a1)*Math.cos(b1);
+    y = Math.cos(b2)*Math.sin(a2) - Math.cos(b1)*Math.sin(b1);
+    z = Math.sin(b2) - Math.sin(b1);
+    
+    //2. Compute chord length
+    c = Math.sqrt((x*x) + (y*y) + (z*z));
+    
+    //3. Compute central angle
+    o = 2*Math.arcsin(c/2);
+    
+    //TODO: Find a way to live update this final step, as it depends on the unit chosen
+    //My implementation might work, but could use some polish I believe
+    //4. Find GCD
+    d = 0;
+    if (unit == "miles"){ d = rmi*o;} //Miles earth radius
+    else{ d = rkm*o;}                 //Kilometer earth radius
+    
+    //5. Return the distance *phew*
+    return d;
+}
+  
+  calc(event) {
+    /* Coordinates are text.  Must use GCD chord formula */
+    this.setState({output : /* just adds the two for now */Number(this.state.source) + Number(this.state.destination)}); 
+  }
 
   render() { /* 2 x 3 table containing source, destination, and output rows*/
     return (
