3fefcd7213b5681b4ccd29ad8931ec99868ce37e
jjpk <jjpk@rams.colostate.edu>
2018-03-04 18:08:05 -0700

    Creating classes to have a more readable and maintainable codebase

---
+++ server/src/test/java/com/tripco/t17/server/TestHTTP.java
@@ -0,0 +1,14 @@
+package com.tripco.t17.server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestHTTP {
+
+    @Test
+    public void testTrue() {
+        // assertTrue checks if a statement is true
+        assertTrue(true == true);
+    }
+}
