6174e70919db9eb3b85e724bfaeff2b7d31bb215
jjpk <jjpk@rams.colostate.edu>
2018-03-27 18:37:50 -0600

    Add nautical miles button and consolidate some code into this.unitsButtons. Also moved this.optCardHeader to the bottom because this.unitsButtons had to be below the other <>.bind things.

--- client/src/Options.js
+++ client/src/Options.js
@@ -9,14 +9,25 @@ import React, {Component} from 'react';
 class Options extends Component {
   constructor(props) {
     super(props);
-    this.optCardHeader = <h5 className="card-header bg-info text-white">
-
-        Options
-      </h5>;
     this.changeUnit = this.changeUnit.bind(this);
     this.retrieveOptimizationValue = this.retrieveOptimizationValue.bind(this);
     this.changeOptimization = this.changeOptimization.bind(this);
     this.retrieveOptimizationString = this.retrieveOptimizationString.bind(this);
+    this.optCardHeader = <h5 className="card-header bg-info text-white">
+      Options
+    </h5>;
+    this.unitsButtons =
+      <div className="btn-group btn-group-toggle" data-toggle="buttons" onChange={this.changeUnit}>
+        <label className={this.testActiveBtn("miles")}>
+          <input type="radio" value="miles" name="distance"/> Miles
+        </label>
+        <label className={this.testActiveBtn("kilometers")}>
+          <input type="radio" value="kilometers" name="distance"/> Kilometers
+        </label>
+        <label className={this.testActiveBtn("nautical miles")}>
+          <input type="radio" value="nautical miles" name="distance"/> Nautical Miles
+        </label>
+      </div>;
   }
 
   /**
@@ -87,14 +98,7 @@ class Options extends Component {
           <div className="col-xs-2 col-sm-6 col-md-4 col-lg-4 col-xl-4">
             <div className="card-body">
               <h6 className="card-title">Unit of distance:</h6>
-              <div className="btn-group btn-group-toggle" data-toggle="buttons" onChange={this.changeUnit}>
-                <label className={this.testActiveBtn("miles")}>
-                  <input type="radio" value="miles" name="distance"/> Miles
-                </label>
-                <label className={this.testActiveBtn("kilometers")}>
-                  <input type="radio" value="kilometers" name="distance"/> Kilometers
-                </label>
-              </div>
+              {this.unitsButtons}
             </div>
           </div>
           <div className="col-xs-2 col-sm-6 col-md-8 col-lg-8 col-xl-8">
@@ -103,7 +107,7 @@ class Options extends Component {
                 <div>
                   <input type="range" className="slider" min="0" max="99" step="1" id="myRange"
                          value={this.retrieveOptimizationValue()} onChange={this.changeOptimization}/>
-                  <h6>Length: <b><big>{this.retrieveOptimizationString()}</big></b></h6>
+                  <h6>Length: <b>{this.retrieveOptimizationString()}</b></h6>
                 </div>
             </div>
           </div>
