78e630768f07ef901256ee2171cafe2bc4164f07
jjpk <jjpk@rams.colostate.edu>
2018-04-02 19:12:26 -0600

    We can pull down a modal to set a new custom unit, and have that unit appear in the dropdown and be selectable! :D There are some known bugs, but they are not something i want to spend hours fixing, I need to get storypoints done :)

--- client/src/Options.js
+++ client/src/Options.js
@@ -5,7 +5,7 @@ import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 /**
  * Options allows the user to change the parameters for planning
- * and rendering the trip map and itinerary.
+ * and rendering the trip map and itinerary. hello.
  * The options reside in the parent object so they may be shared with the Trip object.
  * Allows the user to set the options used by the application via a set of buttons.
  */
